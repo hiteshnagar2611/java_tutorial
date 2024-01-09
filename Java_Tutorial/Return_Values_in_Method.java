0f251b09d4f339dc40290d22a3e943cb47d076ef
Hitesh Nagar <74867491+hiteshnagar2611@users.noreply.github.com>
2024-01-09 13:54:56 +0000

    .

---
+++ Java_Tutorial/Return_Values_in_Method.java
@@ -0,0 +1,11 @@
+
+public class Return_Values_in_Method {
+    static int myMethod(int x) {
+      return 5 + x;
+    }
+  
+    public static void main(String[] args) {
+      System.out.println(myMethod(3));
+    }
+  }
+  // Outputs 8 (5 + 3)
